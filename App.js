0f5dc518efdef968138867dfd6d3352a3da928fe
Katakuranatsumi <usako7233@gmail.com>
2019-01-24 13:44:57 +0900

    2018年1月24日 AC、C、+ボタンの追加

--- App.js
+++ App.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar, TouchableOpacity } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
+// ボタンのFunctional Component
+const CalcButton = (props) => {
+  const flex = props.flex ? props.flex : 1
+  return (
+  <TouchableOpacity
+   style={[styles.calcButton, {flex: flex}]}
+   onPress={() => {props.btnEvent()}}>
+  <Text style={styles.calcButtonText}>{props.label}</Text>
+  </TouchableOpacity>
+  )
+}
+
 export default class App extends React.Component {
+
+// ボタンの役割ごとに関数を作成
+
+valueButton = (value) => {
+
+}
+
+enterButton = () => {
+
+}
+
+calcButton = (value) => {
+
+}
+
+acButton = () => {
+
+}
+
+cButton = () => {
+
+}
+
   render() {
     return (
       <View style={styles.container}>
@@ -15,7 +50,12 @@ export default class App extends React.Component {
         </View>
         {/* ボタンを配置するView */}
         <View style={styles.buttons}>
-          <View style={styles.buttonsLine}></View>
+          <View style={styles.buttonsLine}>
+          {/* ボタンを配置 */}
+          <CalcButton flex={2} label={'AC'} btnEvent={() => this.acButton()}/>
+          <CalcButton label={'c'} btnEvent={() => this.cButton()}/>
+          <CalcButton label={'+'} btnEvent={() => this.calcButton('+')} />
+          </View>
           <View style={styles.buttonsLine}></View>
           <View style={styles.buttonsLine}></View>
           <View style={styles.lastButtonLinesContainer}>
@@ -72,5 +112,17 @@ const styles = StyleSheet.create({
    flex: 1,
    alignItems: 'center',
    borderWidth: 1,
+  },
+  calcButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: "#b0c4de",
+  },
+  calcButtonText: {
+    fontSize: 20,
   },
 });
